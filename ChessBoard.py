c24bbeef74970bff85a44a3c4f91d676838b479d
Divya Prakash Jha <prakashdivya0607@gmail.com>
2023-03-19 17:44:57 +0530

    Added ChessBoard

---
+++ ChessBoard.py
@@ -0,0 +1,24 @@
+from Pieces import *
+
+# Square size
+side = 100
+
+# Colors
+black = (110, 110, 110); # to depict black
+white = (255, 255, 255);
+
+# Board Size
+height = side*8
+width = side*12
+
+# Board State
+BoardState = [
+    [rookW1, knightW1, bishopWw, queenW, kingW, bishopWb, knightW2, rookW2],
+    [pawnW1, pawnW2, pawnW3, pawnW4, pawnW5, pawnW6, pawnW7, pawnW8],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [null, null, null, null, null, null, null, null],
+    [pawnB1, pawnB2, pawnB3, pawnB4, pawnB5, pawnB6, pawnB7, pawnB8],
+    [rookB1, knightB1, bishopBb, queenB, kingB, bishopBw, knightB2, rookB2]
+]
