b20688ee6a0ff569bf15dd04b986197d32d9f9ed
Divya Prakash Jha <prakashdivya0607@gmail.com>
2023-03-24 19:18:40 +0530

    Minor updates

--- VoiceRecognition.py
+++ VoiceRecognition.py
@@ -1,9 +1,24 @@
-import pyaudio
-import speech_recognition
+import speech_recognition as sr
+import time
+
+# def speechToText():
+    # recognizer = sr.Recognizer()
+    #
+    # try:
+    #     with sr.Microphone() as mic:
+    #         print("Speak..")
+    #         recognizer.adjust_for_ambient_noise(mic, duration = 0.1)
+    #         audio = recognizer.listen(mic)
+    #         print("Recognizing..")
+    #         text = recognizer.recognize_ibm(audio)
+    #
+    #         return text
+    # except:
+    #     return "zz"
 
 
 def getPosByVoice(turn, gameState):
     if (gameState == 0):
         return (1, 5)
     else:
-        return (4, 5)
+        return (2, 5)
