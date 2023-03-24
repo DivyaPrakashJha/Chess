03ef9da56316dad1871ccef9adf8dcc52c190094
Divya Prakash Jha <prakashdivya0607@gmail.com>
2023-03-24 19:04:30 +0530

    generated possible moves

--- ChessEngine.py
+++ ChessEngine.py
@@ -1,21 +1,25 @@
 from ChessBoard import BoardState
 from Pieces import *
 
-def implementMove(initPos, finalPos):
-    pieceAtInitPos = BoardState[initPos[0]][initPos[1]]
-    pieceAtFinalPos = BoardState[finalPos[0]][finalPos[1]]
+from Rules import *
 
-    if pieceAtInitPos.isValidMove():
-        pieceAtInitPos.updateCurrPos( (finalPos[1], finalPos[0]) );
+def implementMove(initPos, finalPos):
+    if isValid(initPos, finalPos, False):
+        pieceAtInitPos = BoardState[initPos[0]][initPos[1]]
+        pieceAtFinalPos = BoardState[finalPos[0]][finalPos[1]]
+
+        pieceAtInitPos.updateCurrPos( (finalPos[1], finalPos[0]) )
         if pieceAtFinalPos == null: # Empty cell
             ( BoardState[initPos[0]][initPos[1]] , BoardState[finalPos[0]][finalPos[1]] ) = (null, BoardState[initPos[0]][initPos[1]] )
         elif pieceAtFinalPos.color != pieceAtInitPos.color: # Capture
             pieceAtFinalPos.updateCurrPos(pieceAtFinalPos.ripPos)
             ( BoardState[initPos[0]][initPos[1]] , BoardState[finalPos[0]][finalPos[1]] ) = (null, BoardState[initPos[0]][initPos[1]] )
 
-def generatePossibleMoves():
+def generatePossibleMoves(initPos):
     moves = []
-    moves.append( (4, 5) )
-    moves.append( (4, 6) )
 
+    for i in range(0, 8) :
+        for j in range(0, 8):
+            if isValid(initPos, (i, j), isUnderCheck()):
+                moves.append((j, i))
     return moves
