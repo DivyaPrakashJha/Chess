ae716f8aace702cafb3eb4cef3f26c06b58ea1c3
Divya Prakash Jha <prakashdivya0607@gmail.com>
2023-05-03 00:53:25 +0530

    Made Board Class

--- ChessBoard.py
+++ ChessBoard.py
@@ -1,18 +1,25 @@
 from Pieces import *
 
+# Colors
+grey = ((110, 110, 110), (255, 255, 255)) # (black, white)
+green = ((119, 149, 86), (235, 236, 208))
+brown = ((181, 136, 99), (240, 217, 181))
+blue = ((107, 149, 187), (200, 222, 253))
+
 # Square size
 side = 90
+class Board:
+    def __init__(self, side, color, height, width):
+        self.side = side
+        self.color = color
+        self.height = height
+        self.width = width
 
-# Colors
-black = (110, 110, 110); # to depict black
-white = (255, 255, 255);
 
-# Board Size
-height = side*8
-width = side*12
+board = Board(side, grey, side*8, side*12)
+
 
 # Board State
-
 BoardState = [
     [rookB1, knightB1, bishopBw, queenB, kingB, bishopBb, knightB2, rookB2],
     [pawnB1, pawnB2, pawnB3, pawnB4, pawnB5, pawnB6, pawnB7, pawnB8],
